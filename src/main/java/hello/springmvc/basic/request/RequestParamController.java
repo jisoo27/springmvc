45031d93a66ea68fdb94e99a0b07e5e1306ee4c6
jisu <jisuuoo96@gmail.com>
2022-02-15 19:46:19 +0900

    Using the @RequestParam provided by Spring (V3) omit @RequestParam() when the name of the http parameter is the same as the name of the variable

--- src/main/java/hello/springmvc/basic/request/RequestParamController.java
+++ src/main/java/hello/springmvc/basic/request/RequestParamController.java
@@ -42,5 +42,19 @@ public class RequestParamController {
         return "ok";
     }
 
+    /**
+     * @RequestParam 사용
+     * HTTP 파라미터 이름이 변수 이름과 같으면 @RequestParam(name="xx") 생략 가능
+     * */
+
+    @ResponseBody
+    @RequestMapping("/request-param-v3")
+    public String requestParamV3(
+            @RequestParam String username,
+            @RequestParam int age) {
+        log.info("username={}, age = {}", username, age);
+        return "ok";
+    }
+
 
 }
