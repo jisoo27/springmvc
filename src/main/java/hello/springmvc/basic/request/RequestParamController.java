0c80415a3d6c76ad4b15c16b3f71bf5237488131
jisu <jisuuoo96@gmail.com>
2022-02-15 19:40:48 +0900

    Using the @RequestParam provided by Spring (V2)

--- src/main/java/hello/springmvc/basic/request/RequestParamController.java
+++ src/main/java/hello/springmvc/basic/request/RequestParamController.java
@@ -4,6 +4,8 @@ package hello.springmvc.basic.request;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -23,4 +25,22 @@ public class RequestParamController {
 
         response.getWriter().write("ok"); // return 타입이 void고 response에 값을 써버리면 값을 볼 수 있다.
     }
+
+    /**
+     * @RequestParam 사용
+     * - 파라미터 이름으로 바인딩
+     * @ResponseBody 추가
+     * - View 조회를 무시하고, HTTP message body에 직접 해당 내용 입력
+     * */
+
+    @ResponseBody // View 조회를 무시하고 , Http message body 에 직접 해당 내용 입력
+    @RequestMapping("/request-param-v2")
+    public String requestParamV2(
+            @RequestParam("username") String memberName,
+            @RequestParam("age") int memberAge) {
+        log.info("username={}, age = {}", memberName, memberAge);
+        return "ok";
+    }
+
+
 }
